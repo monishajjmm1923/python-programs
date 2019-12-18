e52f40b7747747e5dd5ffda65f499608d92c0a3f
unknown <WindowsP141@creation-118.Win10.com>
2019-12-18 17:33:25 +0530

    execute a block of statements

---
+++ loops.py
@@ -0,0 +1,38 @@
+#print multiplication table using for loop
+
+a=int(input("enter the table"))
+for i in range(1,11):
+    print("%dx%d= %d" %(a,i,a*i))
+
+#while loop
+i=1
+while i<10:
+    print(i);
+    i+=1;
+
+#break stmt
+
+i=1
+while i<10:
+    print(i)
+    if i==2:
+        break
+    i += 1
+
+#continue stmt
+for a in "python":
+    if a=="h":
+        continue
+    print(a)
+
+#pass stmt
+
+for a in [1,2,3,4,5]:
+    if a==2:
+        pass
+    print a
+
+
+
+
+
