ac69f7f36bfaed3c7d3117445ae5b0c9962388f6
unknown <WindowsP141@creation-118.Win10.com>
2019-12-19 18:20:23 +0530

    used to install and manage software packages

---
+++ pip.py
@@ -0,0 +1,17 @@
+#type the command in terminal as pip install pakagename
+
+import camelcase
+
+c = camelcase.CamelCase()   #This method capitalizes the first letter of each word.
+
+txt = "stay positive"
+
+print(c.hump(txt))
+
+
+#Use the uninstall command to remove a package
+#pip uninstall camelcase
+
+
+#Use the list command to list all the packages installed on your system
+#pip list
